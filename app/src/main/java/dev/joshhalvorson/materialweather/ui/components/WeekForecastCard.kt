d99f8db17900a7d987deb7af11516665e03764a2
JoshHalvorson <joshhalvorson16@gmail.com>
2023-11-11 21:04:03 -0800

    Change hourly forecast into a list instead of a row

--- app/src/main/java/dev/joshhalvorson/materialweather/ui/components/WeekForecastCard.kt
+++ app/src/main/java/dev/joshhalvorson/materialweather/ui/components/WeekForecastCard.kt
@@ -1,6 +1,7 @@
 package dev.joshhalvorson.materialweather.ui.components
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,27 +13,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import dev.joshhalvorson.materialweather.R
+import dev.joshhalvorson.materialweather.data.models.weather.Day
 import dev.joshhalvorson.materialweather.data.models.weather.ForecastResponse
 import dev.joshhalvorson.materialweather.data.models.weather.Forecastday
 import dev.joshhalvorson.materialweather.data.models.weather.Hour
@@ -85,49 +91,116 @@ private fun DayItem(wrapper: Forecastday, expanded: Boolean, onClicked: () -> Un
 
 @Composable
 private fun HourlyForecast(wrapper: Forecastday) {
-    Column {
-        LazyRow(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(items = wrapper.hour, key = { it.timeEpoch }) {
-                HourlyItem(hourly = it)
-            }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .height(250.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items = wrapper.hour, key = { it.timeEpoch }) {
+            HourlyItem(dayForecastProvider = { wrapper.day }, hourlyProvider = { it })
         }
     }
 }
 
 @Composable
-private fun HourlyItem(hourly: Hour) {
-    Surface(
-        shape = RoundedCornerShape(8.dp),
-        color = MaterialTheme.colorScheme.inversePrimary
+private fun HourlyItem(dayForecastProvider: () -> Day, hourlyProvider: () -> Hour) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(
-            modifier = Modifier.padding(4.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            AsyncImage(
-                modifier = Modifier.size(16.dp),
-                model = hourly.condition.getIconUrl(),
-                contentDescription = null
-            )
+        AsyncImage(
+            modifier = Modifier.size(16.dp),
+            model = hourlyProvider().condition.getIconUrl(),
+            contentDescription = null
+        )
 
-            // TODO add metric support
+        // TODO add metric support
+        Text(
+            text = hourlyProvider().getCurrentTempF(),
+            style = MaterialTheme.typography.labelSmall
+        )
+
+        Box(
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            PrecipitationChance(chanceOfRain = hourlyProvider().getChanceOfRain(), small = true)
+
+            PrecipitationChance(
+                placeholder = true,
+                chanceOfRain = "100%",
+                small = true
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.CenterEnd
+        ) {
             Text(
-                text = hourly.getCurrentTempF(),
+                text = hourlyProvider().getTimeDisplay(),
                 style = MaterialTheme.typography.labelSmall
             )
 
-            PrecipitationChance(chanceOfRain = hourly.getChanceOfRain(), small = true)
-
+            // Placeholder
             Text(
-                text = hourly.getTimeDisplay(),
+                modifier = Modifier
+                    .alpha(0f)
+                    .semantics { contentDescription = "" },
+                text = "12:00 AM",
                 style = MaterialTheme.typography.labelSmall
             )
         }
+
+        Spacer(Modifier.width(20.dp))
+
+        // TODO add metric support
+        TemperatureBar(
+            dailyMaxTemperature = dayForecastProvider().maxtempF.toInt(),
+            hourTemperature = hourlyProvider().tempF.toInt()
+        )
+    }
+}
+
+@Composable
+private fun TemperatureBar(dailyMaxTemperature: Int, hourTemperature: Int) {
+    val percentToFill by remember {
+        derivedStateOf {
+            val float = if (dailyMaxTemperature == 0) {
+                0f
+            } else {
+                (hourTemperature.toFloat() / dailyMaxTemperature.toFloat()) * 100f
+            }
+
+            if (float == 100f) {
+                1f
+            } else if (float != 0f) {
+                ".${float.toString().replace(".", "")}".toFloat()
+            } else {
+                0f
+            }
+        }
+    }
+
+    Box {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+                .background(color = MaterialTheme.colorScheme.outlineVariant)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(percentToFill)
+                .height(10.dp)
+                .align(Alignment.CenterEnd)
+                .background(color = MaterialTheme.colorScheme.primary.copy(alpha = percentToFill))
+        )
     }
 }
 
@@ -171,8 +244,19 @@ private fun DayForecast(dayWeather: Forecastday) {
 }
 
 @Composable
-private fun PrecipitationChance(chanceOfRain: String, small: Boolean = false) {
+private fun PrecipitationChance(
+    placeholder: Boolean = false,
+    chanceOfRain: String,
+    small: Boolean = false
+) {
+    val textModifier = if (placeholder) {
+        Modifier.semantics { contentDescription = "" }
+    } else {
+        Modifier
+    }
+
     Row(
+        modifier = if (placeholder) Modifier.alpha(0f) else Modifier,
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(2.dp)
     ) {
@@ -183,7 +267,9 @@ private fun PrecipitationChance(chanceOfRain: String, small: Boolean = false) {
             tint = MaterialTheme.colorScheme.onPrimaryContainer
         )
         Text(
-            modifier = Modifier.defaultMinSize(minWidth = if (!small) 40.dp else 20.dp),
+            modifier = Modifier
+                .defaultMinSize(minWidth = if (!small) 40.dp else 20.dp)
+                .then(textModifier),
             text = chanceOfRain,
             style = if (!small) LocalTextStyle.current else MaterialTheme.typography.labelSmall,
             textAlign = TextAlign.Start
