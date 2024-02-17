7762943562f9b3b9af253141f39eccd4456a673d
JoshHalvorson <joshhalvorson16@gmail.com>
2024-02-17 10:42:19 -0800

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            isDebuggable = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -101,7 +100,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-tooling-preview")
