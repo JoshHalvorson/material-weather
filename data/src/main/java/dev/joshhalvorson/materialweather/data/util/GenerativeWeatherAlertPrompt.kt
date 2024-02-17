27569757e2cdb9a4add63b67ab85634c083db0c3
JoshHalvorson <joshhalvorson16@gmail.com>
2024-02-17 13:26:50 -0800

    Update prompt

--- data/src/main/java/dev/joshhalvorson/materialweather/data/util/GenerativeWeatherAlertPrompt.kt
+++ data/src/main/java/dev/joshhalvorson/materialweather/data/util/GenerativeWeatherAlertPrompt.kt
@@ -6,8 +6,9 @@ fun getGenerativeWeatherAlertPrompt(tomorrowsWeather: Day, unit: String): String
     return """
         Can you give me an interesting piece of information in tomorrows weather ($tomorrowsWeather) 
         in $unit units, ignoring the high and low temperatures, rain, and snow, phrased as a sentence. 
-        Also please round any number with a decimal to the nearest whole number. 
+        Also please round any number with a decimal to the nearest whole number. Don't include bullet points
         
         Can you also give some advice on being ready for tomorrows weather, phrased in a helpful way.
+         You can include bullet points for this
     """.trimIndent()
 }
