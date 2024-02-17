58fb12a7e46296c32fdb9d8038e39efa980c99ba
JoshHalvorson <joshhalvorson16@gmail.com>
2024-02-17 13:07:40 -0800

    Incrment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
