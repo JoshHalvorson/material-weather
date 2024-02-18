b9fad10dd47bf186c95bcc14a39461fca6310566
JoshHalvorson <joshhalvorson16@gmail.com>
2024-02-17 20:21:46 -0800

    Update prompt

--- data/src/main/java/dev/joshhalvorson/materialweather/data/util/GenerativeWeatherAlertPrompt.kt
+++ data/src/main/java/dev/joshhalvorson/materialweather/data/util/GenerativeWeatherAlertPrompt.kt
@@ -3,11 +3,11 @@ package dev.joshhalvorson.materialweather.data.util
 import dev.joshhalvorson.materialweather.data.models.weather.Day
 
 fun getGenerativeWeatherAlertPrompt(tomorrowsWeather: Day, unit: String): String {
-    // TODO specify during the day
     return """
         Can you give me an interesting piece of information in tomorrows weather ($tomorrowsWeather) 
-        in $unit units, ignoring the high and low temperatures, rain, and snow, phrased as a sentence. 
-        Also please round any number with a decimal to the nearest whole number. Don't include bullet points
+        in $unit units. Only using the weather during the day, ignoring the high and low temperatures,
+        rain, and snow, phrased as a sentence. Also please round any number with a decimal to the
+         nearest whole number. Don't include bullet points
         
         Can you also give some advice on being ready for tomorrows weather, phrased in a helpful way.
          You can include bullet points for this
